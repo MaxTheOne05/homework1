0fbd0caa0c7a1474838c27f822828dce337d8eb9
giammo <giammaria.2108040@studenti.uniroma1.it>
2025-04-22 20:36:07 +0200

    Ho diviso i compiti di fai_tutto in diverse funzioni con il solo scopo di rendere il tutto piu chiaro. (aggiornato anche progetto.h di conseguenza)

--- funzioni.c
+++ funzioni.c
@@ -9,49 +9,45 @@
 
 int fai_tutto(char *in_filename, char *out_filename){
 
-	//apriamo direttamente in_filename perche possiamo assumere che sia nella CWD
-	FILE *fi = fopen(in_filename, "r");
-	if (fi == NULL){
-		fprintf(stderr, "Errore in apertura! Impossibile aprire il file di input: %s\n", in_filename);
-		exit(1);
-		return 1;
-	}
-
-    //apriamo il file di output se necessario, altrimenti è stdout
-	FILE *fout = stdout;
-	if (out_filename != NULL){
-		fout = fopen(out_filename, "w");
-		if (fout == NULL){
-			fprintf(stderr, "Errore apertura file output: %s\n", out_filename);
-			exit(1);
-    	}
-	}
+    //apriamo direttamente in_filename perche possiamo assumere che sia nella CWD
+	FILE *fi = apri(in_filename);
 
     //leggiamo il contenuto del file di input. La variabile conterrà il puntatore al primo carattere del file letto
     char *testo = leggi(fi);
 
     //ci prendiamo la directory in cui sono salvati tutti i file .h e .c da includere
-    char copia[2048];
-    strncpy(copia, in_filename, 2048);  //è importante fare la copia perche dirname() modifica la stringa
-    char *input_dir = dirname(copia);   //estrae il path della cartella dal path del file
+    char *input_dir = prendi_nomedir(in_filename);
 
+    //finche ci sono include VALIDI nel testo li rimuoviamo
     while (conta_include(testo)>0){
-        //inserire la funzione che rimuove i commenti
+        //inserire la funzione che rimuove i commenti       //per evitare di risolvere include commentati andiamo prima a rimuovere i commenti
         char *tmp = risolvi_includes(testo, input_dir);     //tmp conterrà il testo aggiornato con gli include risolti
         free(testo);                                        //boh da vedere :)
         testo = tmp;
     }
     
-    scrivi(out_filename, testo);         //ritorna 0 se va tutto bene, 1 se c'è errore in scrittura
+    //scriviamo il risultato nel file di output o nello stdout
+    scrivi(out_filename, testo);
+    
+    //liberiamo lo spazio allocato e ritorniamo 0 (esecuzione terminata correttamente)
     free(testo);
-
     return 0;
 }
 
-
+//Dato il nome del file lo apre in sola lettura
+FILE *apri (char *filename){
+	FILE *fi = fopen(filename, "r");
+	if (fi == NULL){
+		fprintf(stderr, "Errore in apertura! Impossibile aprire il file: %s\n", filename);
+		exit(1);
+	}
+    return fi;
+}
 
 //Dato il nome di un file lo apre, richiama la funzione leggi e ne restituisce il contenuto letto
 char *leggi_da_filename(char *filename){
+
+    //QUI UTILIZZARE LA FUNZIONE apri. Per ora la lascio cosi perche è piu facile per debug.
     FILE *f = fopen(filename, "r");
     if (f == NULL) {
         fprintf(stderr, "<leggi_da_filename>: Errore apertura file: %s\n", filename);
@@ -62,10 +58,8 @@ char *leggi_da_filename(char *filename){
     return testo;
 }
 
-
+//Dato il puntatore ad un file lo apre e ne restituisce TUTTO il contenuto in una stringa
 char *leggi(FILE *fi){
-    //inizialmente leggiamo tutto il testo in input. Ignorando la risoluzione di include e rimozione commenti.
-    //(quello lo faremo piu avanti e salveremo il risultato in un'altra stringa)
     
     size_t lenTesto = 1;                //lunghezza "dinamica" del file in input. Partiamo da 1 per includere '\0'
     char *testo = malloc(lenTesto);     //iniziamo a creare la stringa dinamica.  
@@ -90,9 +84,16 @@ char *leggi(FILE *fi){
     return testo;
 }
 
+//Dato il filename estrae il nome della directory (serve per formare il path assoluto dei file da includere)
+char *prendi_nomedir(char *filename) {
+    char copia[2048];
+    strncpy(copia, filename, 2048);     //è importante fare la copia perche dirname() modifica la stringa
+    return strdup(dirname(copia));      //estrae il path della cartella dal path del file. 
+                                        //Senza strdup il puntatore a "copia" verrebbe cancellato dopo il return. Quindi la stringa ritornata sarebbe ""
+}
+
 //Risolve tutti gli include con "" presenti in testo
 char* risolvi_includes(char *testo, char *input_dir) {
-                                            //similmente a quanto fatto con leggi allochiamo dinamicamente la memoria
     size_t lenResult = 1;                   //inizialmente vale solo 1 perche contiene solo il terminatore '\0'
     size_t write_pos = 0;                   //ci dice il prossimo punto in cui andremo a scrivere
     char *result = malloc(lenResult);       
@@ -116,14 +117,14 @@ char* risolvi_includes(char *testo, char *input_dir) {
 
         } else {
 
-            //se i prossimi char sono #include saltiamo l'intera parola gli spazi per cercare "" oppure <>
-            char *p = read_pos + 8;
-            while (isspace(*p)){     
+            //Andiamo cercare "" oppure <>
+            char *p = read_pos + 8;     //saltiamo la parola #include
+            while (isspace(*p)){        //saltiamo tutti gli spazi
                 p++;                            
             }
             
             if (*p == '"'){
-                //CASO 2: è un include "". Apriamo il file e ne compia l'intero contenuto in result
+                //CASO 2: è un #include "filename". Apriamo il file e ne compia l'intero contenuto in result
                 char filename[1024];                    //conterrà il nome del file
                 int i = 0;                              //ci serve per scorrere filename
 
@@ -137,25 +138,25 @@ char* risolvi_includes(char *testo, char *input_dir) {
                 //concateniamo input_dir e filename 
                 char fullpath[2048];
                 snprintf(fullpath, sizeof(fullpath), "%s/%s", input_dir, filename);
-                //NON possiamo usare strcat perche andrebbe a modificare direttamente dir_name, che invece va lasciata intatta (poiche ci servirà per gli altri file da includere)
+                //NON possiamo usare strcat perche andrebbe a modificare direttamente dir_name, che invece va lasciata intatta 
+                //(poiche ci servirà per gli altri file da includere)
 
+                //Apriamo il file e ne prendiamo il contenuto
                 char* included_content = leggi_da_filename(fullpath);   //prendiamo il contenuto del file da includere (il caso di fallimento è gia gestito in leggi())
                 size_t lenTesto = strlen(included_content);             //verifichiamo che ci sia abbastanza spazio per scrivere
 
-                size_t spazio_necessario = write_pos+lenTesto+1;        //dobbiamo scrivere: tutto il testo fino ad ora + il testo dell'include + \0
+                size_t spazio_necessario = write_pos+lenTesto+1;        //dobbiamo scrivere: testo fino ad ora + testo dell'include + \0
                 while (spazio_necessario > lenResult) {
                     lenResult *= 2;                                     //se non c'è spazio lo creiamo (aumentando esponenzialmente, come prima)
                 }
                 result = safe_realloc(result, lenResult);
                 memcpy(result + write_pos, included_content, lenTesto); //aggiungiamo il contenuto a result
-                write_pos += lenTesto;
-                result[write_pos] = '\0';
-                free(included_content);
-                //liberiamo lo spazio di included_content poiche abbiamo copiato in result e terminato   
+                write_pos += lenTesto;                                  //spostiamo il prossimo punto in cui scrivere
+                result[write_pos] = '\0';                               //inseriamo il terminatore di stringa
+                free(included_content);                                 //liberiamo lo spazio di included_content poiche abbiamo copiato in result   
 
             } else {
-                //CASO 3: è un include <>. Copiamo tutto.
-
+                //CASO 3: è un #include <filename>. Copiamo il primo carattere e leggiamo il prossimo carattere
                 if (write_pos + 1 > lenResult) {            //se non c'è abbastanza spazio per aggiungere un nuovo char
                     lenResult *= 2;                         //aumentiamo lo spazio allocato esponenzialmente (per maggior efficienza)
                     result = safe_realloc(result, lenResult);
@@ -170,17 +171,14 @@ char* risolvi_includes(char *testo, char *input_dir) {
     return result;
 }
 
-void fai_verbose(){
-    
-}
-
+//Conta il numero di include VALIDI ancora da risolvere
 int conta_include(char *testo) {
-    int count = 0;
-    char *pos = testo;
+    int count = 0;          //conterra il numero di include da risolvere
+    char *pos = testo;      //iniziamo a scorrere il file dall'inizio
 
-    //trova la prima occorrenza di #include nel testo (pos puntera al primo carattere di #include, quindi a #)
+    //strstr trova la prima occorrenza di #include nel testo (pos puntera al primo carattere di #include, quindi a #)
     while ((pos = strstr(pos, "#include ")) != NULL) {
-        pos += 9;                   //saltiamo la parola #include
+        pos += 8;                   //saltiamo la parola #include
         while (isspace(*pos)) {     //salta tutti gli spazi dopo la parola #include
             pos++;
         }
@@ -193,23 +191,24 @@ int conta_include(char *testo) {
 }
 
 //effettua comunque un realloc ma gestendo meglio i casi in cui realloc fallisca
-char* safe_realloc(char* ptr, size_t new_size) {
-    char* tmp = realloc(ptr, new_size);
+char* safe_realloc(char* testo, size_t new_size) {
+    char* tmp = realloc(testo, new_size);
     if (!tmp) {
-        free(ptr);
+        free(testo);
         return NULL;
     }
     return tmp;
 }
 
+//Scrive "testo" nel file "out_filename" (se presente) oppure in stdout (se out_filename == NULL)
 int scrivi(char *out_filename, char *testo) {
-    // Se out_filename è NULL, scriviamo su stdout con printf
+    //se out_filename è NULL, scriviamo su stdout con printf
     if (out_filename == NULL) {
         printf("%s", testo);
         return 0;
     }
 
-    //Se abbiamo un file di utput lo apriam0
+    //se invece abbiamo un file di output lo apriamo
     FILE *fout = fopen(out_filename, "w");
     if (fout == NULL) {
         fprintf(stderr, "Errore apertura file output: %s\n", out_filename);
@@ -220,12 +219,20 @@ int scrivi(char *out_filename, char *testo) {
     size_t written = fwrite(testo, 1, len, fout);   //fwrite restituisce il numero di elementi effettivamente scritti
     fclose(fout);                                   //chiudiamo il file
 
-    //Se il numero di elementi scritti != elementi da scrivere (ne abbiamo persi alcuni) abbiamo un errore
+    //Se il numero di elementi scritti != elementi da scrivere abbiamo un errore
     if (written != len) {
         fprintf(stderr, "Errore scrittura file output: %s\n", out_filename);
         return 1;
     }
 
-    //se invece abbiamo sicrtto tutto correttamente possiamo ritornare 0
+    //se invece abbiamo scritto tutto correttamente possiamo ritornare 0
     return 0;
 }
+
+
+
+void fai_verbose(){
+    
+}
+
+
--- progetto.h
+++ progetto.h
@@ -4,12 +4,14 @@
 #include <stdio.h>
 
 int fai_tutto(char *in_filename, char *out_filename);
+FILE *apri (char *filename);
 char *leggi_da_filename(char *filename);
 char *leggi(FILE *fi);
+char *prendi_nomedir(char *filename);
 char* risolvi_includes(char *testo, char *input_dir);
-void fai_verbose();
 int conta_include(char *testo);
-char* safe_realloc(char* ptr, size_t new_size);
+char* safe_realloc(char* testo, size_t new_size);
 int scrivi(char *out_filename, char *testo);
+void fai_verbose();
 
 #endif
